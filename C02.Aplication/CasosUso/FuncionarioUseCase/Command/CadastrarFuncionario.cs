ba376b05b7e0c2f1ee80fadf1e12344af153b6b8
Gilipo <xaviergilson343@gmail.com>
2026-04-25 15:13:28 +0100

    Bateu

--- C02.Aplication/CasosUso/FuncionarioUseCase/Command/CadastrarFuncionario.cs
+++ C02.Aplication/CasosUso/FuncionarioUseCase/Command/CadastrarFuncionario.cs
@@ -4,21 +4,3 @@ using Corretora.C01.Domain.Interfaces;
 using Corretora.C02.Aplication.CasosUso.FuncionarioUseCase.DTOs;
 
 namespace Corretora.C02.Aplication.CasosUso.FuncionarioUseCase.Command;
-
-public class CadastrarFuncionario(
-    IPesquisarTelefoneRepositorio<tb04_funcionarioModel> pesquisar,
-    ICadastrarRepositorio<tb04_funcionarioModel> cadastrar)
-{
-    public async Task<(CadastrarFuncionarioDTO? dados, string mensagem, int codigo)> Executar(CadastrarFuncionarioDTO dto)
-    {
-        var model = new tb04_funcionarioModel
-        {
-            Nome = dto.FuncionarioNome,
-            Telefone = dto.FuncionarioTelefone,
-            Perfil = dto.FuncionarioIdPerfil
-
-        };
-
-        var (dado, mensagem, codigo) = await cadastrar.CadastrarAsync(model);
-    }
-}
